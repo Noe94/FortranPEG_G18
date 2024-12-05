8d05ff67d640b577bbcfb51e255a8b90126415ab
ferand17 <elderandrade064@gmail.com>
2024-12-04 21:31:47 -0600

    prueba 4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ya funciono prueba 3.
+          Ya funciono prueba.
         </p>
       </header>
     </div>
